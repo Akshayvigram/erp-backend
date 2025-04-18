3795d79c5cb72c453d2d8f3a1ea8a7c27397b8a7
MouleeswaranR <ffmouleesh@gmail.com>
2025-04-18 12:16:55 +0530

    change

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -33,11 +33,7 @@ export const login = async (req, res) => {
      return res.status(200).json({
        success: true,
        token,
-       user: {
-         _id: user._id,
-         name: user.name,
-         role: user.role
-       }
+       user:{_id:user._id,name:user.name,role:user.role}
      });
    } catch (error) {
      console.log(error.message);
