76d17154f572707226f450df01bb5e7ec0954429
MouleeswaranR <ffmouleesh@gmail.com>
2025-04-18 11:24:14 +0530

    backend commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import authRoutes from "./routes/auth.route.js"
+import departmentRoutes from "./routes/department.js"
+import { connectToDataBase } from './db/db.js';
+import employeeRouter from "./routes/employee.js"
+import leaveRouter from './routes/leave.js'
+import settingRoute from './routes/setting.js'
+import attendanceRoute from './routes/attendance.js'
+import dashboardRouter from './routes/dashboard.js'
+import SalaryRouter from "./routes/salary.js"
+
+const app=express();
+app.use(express.static('public/uploads'))
+app.use(cors({
+    origin: 'http://localhost:5173', // Change to your frontend URL
+    credentials: true
+}));
+app.use(express.json());
+app.use("/api/auth",authRoutes)
+app.use("/api/department",departmentRoutes)
+app.use("/api/employee",employeeRouter)
+app.use("/api/salary",SalaryRouter);
+app.use("/api/leave",leaveRouter);
+app.use("/api/setting",settingRoute);
+app.use("/api/dashboard",dashboardRouter);
+
+app.listen(process.env.PORT,()=>{
+    connectToDataBase();
+    console.log(`server running on ${process.env.PORT}`);
+    
+})
